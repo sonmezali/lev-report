87d4049bbd60a569b1061f88352296e13c74b24a
Robin Knipe <robin.knipe@gmail.com>
2019-05-09 17:16:46 +0100

    A basic PoC server app to get graph data

---
+++ src/server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+global.Promise = require('bluebird');
+const fs = require('fs');
+const path = require('path');
+const config = require('./config');
+
+const restify = require('lev-restify');
+const server = restify.createServer(config.name);
+server.errors = restify.errors;
+global.logger = server.log;
+require('./lib/routes')(server);
+
+const bundle = fs.readFileSync(path.resolve('public/js/bundle.js'));
+server.get('/public/js/bundle.js', (req, res) => {
+  res.end(bundle);
+});
+
+const index = fs.readFileSync(path.resolve('pages/index.html'));
+server.get('/', (req, res) => {
+  res.end(index);
+});
+
+server.listen(config.http.port, config.http.host, () => {
+  global.logger.info('%s listening at %s', server.name, server.url);
+});
