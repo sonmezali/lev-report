1fd29e38422b924159eb2b6725b49b9724e7aa6f
Robin Knipe <robin.knipe@gmail.com>
2019-08-12 13:18:39 +0100

    General reworking of DB queries
     - make "to" date constraint a reusable constant
     - improve the query names to better describe their use
     - improve error messages
     - add query to get total usage for each datatype
     - add query to get group usage for each datatype

--- src/lib/db/query.js
+++ src/lib/db/query.js
@@ -2,19 +2,61 @@
 
 const db = require('./postgres');
 
-const baseQuery = 'SELECT date_time::date as date, dataset, count(*) from lev_audit WHERE date_time > $1';
-const grouping = ' group by date_time::date, dataset order by date_time::date';
+const groups = 'SELECT DISTINCT groups FROM lev_audit';
+const countsByDateType = 'SELECT date_time::DATE AS date, dataset, count(*)::INTEGER FROM lev_audit WHERE date_time > $1';
+const countsByType = 'SELECT dataset, count(*)::INTEGER FROM lev_audit WHERE date_time > $1';
+const countsByUser =
+  'SELECT date_time::DATE AS date, dataset, username, count(*)::INTEGER FROM lev_audit WHERE date_time > $1';
+const until = 'AND date_time < $2';
+const groupByDateType = ' GROUP BY date_time::date, dataset ORDER BY date_time::date';
+const groupByType = ' GROUP BY dataset';
+const groupByDateTypeUser = ' GROUP BY date_time::date, dataset, username ORDER BY date_time::date';
+const groupByTypeGroup = 'GROUP BY name, dataset';
+
+const buildCountsByGroup = (from, to, includeNoGroup = true) => `
+SELECT name, dataset, SUM(count)::INTEGER AS count
+FROM (
+  SELECT UNNEST(groups) AS name, dataset, COUNT(*) 
+    FROM lev_audit 
+    WHERE date_time > $1 ${to ? until : ''} 
+    ${groupByTypeGroup} ${includeNoGroup ? `
+  UNION
+  SELECT 'No group' AS name, dataset, COUNT(*) 
+    FROM lev_audit 
+    WHERE groups='{}' AND date_time > $1 ${to ? until : ''}` : ''}
+    ${groupByTypeGroup}
+) AS counts
+${groupByTypeGroup}
+ORDER BY name~'^/Team' desc, name`;
 
 module.exports = {
-  usageSinceDate: (from) => db.manyOrNone(`${baseQuery} ${grouping}`, from)
+  usageByDateType: (from, to) => db.manyOrNone(
+    `${countsByDateType} ${to ? until : ''} ${groupByDateType}`,
+    to ? [from, to] : from)
     .catch(e => {
-      global.logger.error(`Problem retrieving usage data since: ${from}`, e);
+      global.logger.error(`Problem retrieving counts for datatypes by day between: ${from} and ${to || 'now'}`, e);
       throw new Error('Could not fetch data');
     }),
 
-  usageDuringDateRange: (from, to) => db.manyOrNone(`${baseQuery} AND date_time < $2 ${grouping}`, [from, to])
+  usageByType: (from , to) => db.manyOrNone(
+    `${countsByType} ${to ? until : ''} ${groupByType}`,
+    to ? [from, to] : from)
     .catch(e => {
-      global.logger.error(`Problem retrieving usage data between: ${from} and ${to}`, e);
+      global.logger.error(`Problem retrieving counts for datatypes between: ${from} and ${to || 'now'}`, e);
       throw new Error('Could not fetch data');
     }),
+
+  usageByGroup: (from, to) => db.manyOrNone(buildCountsByGroup(from, to), to ? [from, to] : from)
+    .catch(e => {
+      global.logger.error(`Problem retrieving counts for groups between: ${from} and ${to || 'now'}`, e);
+      throw new Error('Could not fetch data');
+    }),
+
+  usageByUser: (from, to) => db.manyOrNone(
+    `${countsByUser} ${to ? until : ''} ${groupByDateTypeUser}`,
+    to ? [from, to] : from)
+    .catch(e => {
+      global.logger.error(`Problem retrieving counts for users between: ${from} and ${to || 'now'}`, e);
+      throw new Error('Could not fetch data');
+    })
 };
