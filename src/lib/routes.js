a20818f6fd30fb34116df1a5c3c302f3d1bb749d
Robin Knipe <robin.knipe@gmail.com>
2019-11-30 19:58:15 +0000

    [LEVWEB-1340] Add routes for the dashboard & data
    New endpoints:
     - `/dashboard` for the dashboard UI
     - `/dashboard/data` for the dashboard JSON data

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -3,7 +3,8 @@
 const dateFormat = 'YYYY-MM-DD HH:mm:ss';
 const moment = require('moment');
 const model = require('./model');
-const report = require('lev-react-components').LevReport;
+const dashboardModel = require('./dashboard-model');
+const { LevDashboard, LevReport } = require('lev-react-components');
 
 module.exports = server => {
   server.get('/readiness', (req, res) => {
@@ -26,11 +27,26 @@ module.exports = server => {
             next(new server.errors.InternalServerError(err));
           });
       }
-
-      next(new server.errors.BadRequestError(`Make sure the date format is '${dateFormat}' (time is optional)`));
+      next(new server.errors.BadRequestError(`Make sure the date format is "${dateFormat}" (time is optional)`));
     } else {
-      next(new server.errors.BadRequestError('Must provide a \'from\' date parameter, and optionally a \'to\' date'));
+      next(new server.errors.BadRequestError('Must provide a "from" date parameter, and optionally a "to" date'));
     }
+  });
+
+  server.get('/dashboard', (req, res) => {
+    dashboardModel()
+      .then(props => res.render(LevDashboard, props))
+      .catch(err => server.log.error(err));
+  });
+
+  server.get('/dashboard/data', (req, res, next) => { // eslint-disable-line consistent-return
+    return dashboardModel()
+      .then(data => JSON.stringify(data, null, 2))
+      .then(res.end.bind(res))
+      .catch(err => {
+        server.log.error(err);
+        next(new server.errors.InternalServerError(err));
+      });
   });
 
   server.get('/*', (req, res) => {
@@ -39,10 +55,10 @@ module.exports = server => {
     const toDate = req.query && req.query.to && moment(req.query.to, dateFormat);
 
     model(fromDate, toDate)
-      .then(props => res.render(report, props))
+      .then(props => res.render(LevReport, props))
       .catch(err => {
         server.log.error(err);
-        res.render(report);
+        res.render(LevReport);
       });
   });
 };
