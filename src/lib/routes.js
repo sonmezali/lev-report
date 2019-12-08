ad1fe21bb95686ff5c74c9d42c9ade364630722f
Robin Knipe <robin.knipe@gmail.com>
2019-12-08 17:43:45 +0000

    [LEVWEB-1340] Refactor DB/model calls into helper

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -6,10 +6,15 @@ const model = require('./model');
 const dashboardModel = require('./dashboard-model');
 const { LevDashboard, LevReport } = require('lev-react-components');
 
-module.exports = server => {
-  server.get('/readiness', (req, res) => {
-    res.send('OK');
+const promiseResponder = (promise, req, res, next, component) => promise
+  .then(data => component ? res.render(component, data) : res.send(data))
+  .catch(err => {
+    req.log.error(err);
+    return next(err);
   });
+
+module.exports = server => {
+  server.get('/readiness', (req, res) => res.send('OK'));
 
   server.get('/data', (req, res, next) => { // eslint-disable-line consistent-return
     const fromDate = req.query && moment(req.query.from, dateFormat);
@@ -19,47 +24,24 @@ module.exports = server => {
       if (!toDate || toDate.isValid()) {
         const from = fromDate.format(dateFormat);
         const to = toDate && toDate.format(dateFormat);
-        return model(from, to)
-          .then(data => JSON.stringify(data, null, 2))
-          .then(res.end.bind(res))
-          .catch(err => {
-            server.log.error(err);
-            next(new server.errors.InternalServerError(err));
-          });
+        return promiseResponder(model(from, to), req, res, next);
       }
-      next(new server.errors.BadRequestError(`Make sure the date format is "${dateFormat}" (time is optional)`));
-    } else {
-      next(new server.errors.BadRequestError('Must provide a "from" date parameter, and optionally a "to" date'));
+      return next(new server.errors.BadRequestError(`Make sure the date format is "${dateFormat}" (time is optional)`));
     }
+    return next(new server.errors.BadRequestError('Must provide "from" date parameter, and optionally a "to" date'));
   });
 
-  server.get('/dashboard', (req, res) => {
-    dashboardModel()
-      .then(props => res.render(LevDashboard, props))
-      .catch(err => server.log.error(err));
-  });
+  server.get('/dashboard', (req, res, next) => promiseResponder(dashboardModel(), req, res, next, LevDashboard));
 
-  server.get('/dashboard/data', (req, res, next) => { // eslint-disable-line consistent-return
-    return dashboardModel()
-      .then(data => JSON.stringify(data, null, 2))
-      .then(res.end.bind(res))
-      .catch(err => {
-        server.log.error(err);
-        next(new server.errors.InternalServerError(err));
-      });
-  });
+  server.get('/dashboard/data', (req, res, next) => promiseResponder(dashboardModel(), req, res, next));
 
-  server.get('/*', (req, res) => {
+  server.get('/*', (req, res, next) => {
     const fromDate = req.query && req.query.from && moment(req.query.from, dateFormat);
     const toDate = req.query && req.query.to && moment(req.query.to, dateFormat);
 
-    model(
+    return promiseResponder(model(
       fromDate.isValid() ? fromDate.format(dateFormat) : moment().startOf('month'),
-      toDate.isValid() && toDate.format(dateFormat))
-      .then(props => res.render(LevReport, props))
-      .catch(err => {
-        server.log.error(err);
-        res.render(LevReport);
-      });
+      toDate.isValid() && toDate.format(dateFormat)
+    ), req, res, next, LevReport);
   });
 };
