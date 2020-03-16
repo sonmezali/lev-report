93383735f669098eb612387c1dd3c61e20c64129
Aaron Gill <aaron.gill@digital.homeoffice.gov.uk>
2020-03-16 12:28:12 +0000

    [LEVWEB-1364] Query for cost saving to customer
    ...with tests and mock data

--- mock/mock-query.js
+++ mock/mock-query.js
@@ -3,10 +3,12 @@ const totals = require('./dataset-totals');
 const groups = require('./group-data');
 const dailySearches = Math.round(Math.random() * 5000);
 const allTime = Math.round(Math.random() * 1000000);
+const allTimeCustomerSearches = Math.round(Math.random() * 100000000);
 
 module.exports = {
   usageByDateType: () => Promise.resolve(daily),
   usageByType: () => Promise.resolve(totals),
   usageByGroup: () => Promise.resolve(groups),
   searchTotals: (isAllTimeCount) => Promise.resolve(isAllTimeCount ? allTime : dailySearches),
+  totalCustomerSearches: () => Promise.resolve(allTimeCustomerSearches)
 };
