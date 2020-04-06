4c7235d4af66859480d291ae0e106b1f87f46140
Robin Knipe <robin.knipe@gmail.com>
2020-04-06 16:33:26 +0100

    [LEVWEB-1374] Doc with comments instead of JSDoc
    ...and more meaningful value names.

--- src/lib/dashboard-model.js
+++ src/lib/dashboard-model.js
@@ -3,23 +3,14 @@
 const config = require('../config');
 const query = config.postgres.mock ? require('../../mock/mock-query') : require('./db/query');
 
-/**
- * Martin's Multiplier `M`, an estimated ratio of relevant searches in the audit table, currently 55%
- * @type {number}
- */
-const M = 0.55;
-/**
- * Cost of a Second Class "Signed for" letter from Royal Mail: £1.81
- * @type {number}
- */
-const p = 1.81;
-const Mp = M * p;
-/**
- * costSaving calculates the approximate cost saving to the public from not having to send certificates by post
- * @param T Total searches done to date
- * @returns {number} the approximate cost saving to the public
- */
-const costSaving = T => Math.round(T * Mp);
+// Martin's Multiplier, an estimated ratio of relevant searches in the audit table, currently 55%
+const RELEVANT_SEARCH_RATIO = 0.55;
+// Cost of a Second Class "Signed for" letter from Royal Mail: £1.81
+const POSTAGE = 1.81;
+const POSTAGE_RATIO = RELEVANT_SEARCH_RATIO * POSTAGE;
+// costSaving takes TotalSearches and calculates the approximate cost saving to the public
+// ...from not having to send certificates by post
+const costSaving = TotalSearches => Math.round(TotalSearches * POSTAGE_RATIO);
 
 const build = () => Promise.join(
   query.searchTotals(true),
@@ -27,7 +18,7 @@ const build = () => Promise.join(
   (allTime, todaySearches) => ({
     allTime,
     todaySearches,
-    customerCostSaving: costSaving(todaySearches)
+    customerCostSaving: costSaving(allTime)
   })
 );
 
