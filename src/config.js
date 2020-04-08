066d10177d90e6e07f169f5a9b25fd1b790f2331
Robin Knipe <robin.knipe@gmail.com>
2020-04-08 19:46:15 +0100

    [LEVWEB-1369] Improve the dev script
    ...to make it easier to run against a DB instead of using the mock code.

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 'use strict';
 
+const defaultsFalse = v => String(v || '').match(/(true|yes|on)/i) !== null;
+
 /* eslint-disable no-process-env */
 module.exports = {
   name: require('../package').name,
   postgres: {
-    mock: process.env.MOCK,
+    mock: defaultsFalse(process.env.MOCK),
     user: process.env.POSTGRES_USER || 'postgres',
     password: process.env.POSTGRES_PASSWORD || 'postgres',
     host: process.env.POSTGRES_HOST || 'localhost',
     port: process.env.POSTGRES_PORT || '5432',
     database: process.env.POSTGRES_DB || 'lev',
-    ssl: process.env.POSTGRES_SSL || false
+    ssl: defaultsFalse(process.env.POSTGRES_SSL)
   },
   http: {
     host: process.env.LISTEN_HOST || '0.0.0.0',
