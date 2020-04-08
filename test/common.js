e9e1b19612b0fed7a5b4074040542fcf6062767a
Robin Knipe <robin.knipe@gmail.com>
2020-04-08 20:00:39 +0100

    [LEVWEB-1369] Add chai utility `each` for arrays
    ...and `npm audit fix` to update the dependency modules.

--- test/common.js
+++ test/common.js
@@ -1,6 +1,6 @@
 'use strict';
 
-global.chai = require('chai').use(require('chai-as-promised')).use(require('sinon-chai'));
+global.chai = require('chai').use(require('chai-as-promised')).use(require('sinon-chai')).use(require('chai-each'));
 global.assert = chai.assert;
 global.expect = chai.expect;
 global.should = chai.should();
